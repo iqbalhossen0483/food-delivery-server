76a0a3fa064a528998f2a4fcfe6af64e3cd3e980
Md Rakib <rayhan.senbag.bd@gmail.com>
2022-03-03 16:05:17 +0600

    lsdkjjnfb

--- firebase/firebase.init.js
+++ firebase/firebase.init.js
@@ -1,12 +1,13 @@
+const serviceAccount = require("../firebase_keys/firebase-private-keys.json");
 const admin = require('firebase-admin/app');
 require("dotenv").config();
 
 
-const serviceAccount = require("../firebase_keys/firebase-private-keys.json");
 
 const initialization = admin.initializeApp({
     credential: admin.cert(serviceAccount),
     databaseURL: `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.wewoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 });
+
 
 module.exports = initialization;
